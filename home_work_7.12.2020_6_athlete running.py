6c8da103dccbf598f5d61403912368c47ea64779
artembigdata <64332640+artembigdata@users.noreply.github.com>
2020-12-24 20:13:38 +0500

    Create home_work_7.12.2020_6_athlete running.py

---
+++ home_work_7.12.2020_6_athlete running.py
@@ -0,0 +1,20 @@
+a = float(2)
+max = 0
+num = float(a)
+day = int(1)
+while num <= 3:
+    num = num * 1.1
+    day = day + 1
+    print(day,"-й день:","{:.2f}".format(num))
+print("will be more 3 kilometers in", day, "days")
+
+
+
+
+
+
+
+
+
+
+
